3436b99b57fa345471540a0080d75f6b63144da5
Leandro Arturi <lea.arturi@gmail.com>
2024-04-14 12:00:13 -0300

    Prisma config

--- src/configs/envs.ts
+++ src/configs/envs.ts
@@ -1,19 +1,25 @@
+import { Logger } from '@nestjs/common';
 import 'dotenv/config';
 import * as joi from 'joi';
 
 interface EnvVars {
   PORT: number;
+  DATABASE_URL: string;
 }
 
 const envsSchema = joi
   .object({
     PORT: joi.number().required(),
+    DATABASE_URL: joi.string().required(),
   })
   .unknown(true);
 
 const { error, value } = envsSchema.validate(process.env);
 
+const logger = new Logger('Main');
+
 if (error) {
+  logger.error(`Config validation error: ${error.message}`);
   throw new Error(`Config validation error: ${error.message}`);
 }
 
@@ -21,4 +27,5 @@ const envVars: EnvVars = value;
 
 export const envs = {
   port: envVars.PORT,
+  databaseUrl: envVars.DATABASE_URL,
 };
